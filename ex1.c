f12365d138d294a4bd91be7f68d510c0b420a217
yin <1206489318@qq.com>
2020-08-27 00:24:02 +0800

    finish heap

--- ex1.c
+++ ex1.c
@@ -51,9 +51,70 @@ void heap_print(int heap[])
     }
 }
 
-void heap_pop()
+/* 向下调整 */
+void heap_adjust_down(int heap[], int cur_pos)
 {
+    int son_left = (2*cur_pos <= heap[0])?(2*cur_pos):0;
+    int son_right = (2*cur_pos+1<=heap[0])?(2*cur_pos+1):0;
+    int max_id = 0;
+
+    if(!son_left && !son_right)
+        return ;
     
+    if(son_left && son_right)
+    {
+        if(heap[son_left] > heap[son_right])
+            max_id = son_left;
+        else
+            max_id = son_right;
+        
+        goto out;
+    }
+
+    max_id = son_left;
+out:
+    heap_swap(heap, cur_pos, max_id);
+    heap_adjust_down(heap, max_id);
+}
+
+void heap_adjust_update(int heap[], int cur_pos, int data)
+{
+    int old_var = heap[cur_pos];
+    heap[cur_pos] = data;
+
+    /* 如果是根节点 */
+    if(1 == cur_pos)
+        return heap_adjust_down(heap, cur_pos);    
+
+    /* 如果是叶子节点 */
+    if(2*cur_pos > heap[0])
+        return heap_adjust_up(heap, cur_pos);
+
+    if(data > old_var)
+        return heap_adjust_up(heap, cur_pos);
+    else if(data < old_var)
+        return heap_adjust_down(heap, cur_pos);
+}
+
+void heap_pop(int heap[], int* top_data)
+{
+    if(0 == heap[0])
+        return ;
+    if(1 != heap[0])
+        heap_swap(heap, 1, heap[0]);
+
+    *top_data = heap[heap[0]];
+    heap[0]--;
+
+    heap_adjust_down(heap, 1);
+}
+
+void heap_delete(int heap[], int cur_pos)
+{
+    int last_var = heap[heap[0]];
+    heap[0]--;
+
+    heap_adjust_update(heap, cur_pos, last_var);
 }
 
 int main()
@@ -70,6 +131,16 @@ int main()
     heap_print(myheap);
 
     heap_insert(myheap, 8);
+    heap_print(myheap);
+
+    int tmp;
+    heap_delete(myheap, 2);
+    heap_print(myheap);
+
+    heap_delete(myheap, 1);
+    heap_print(myheap);
+
+    heap_delete(myheap, 2);
     heap_print(myheap);
 
     return 0;
